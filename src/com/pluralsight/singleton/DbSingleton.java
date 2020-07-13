22f580ae05bbc2431d3ba9c05cd7e8afc9c44730
DominasPL <dominik-stepuch@wp.pl>
2020-07-13 12:05:13 +0200

    Added thread safe mechanism

--- src/com/pluralsight/singleton/DbSingleton.java
+++ src/com/pluralsight/singleton/DbSingleton.java
@@ -4,13 +4,21 @@ import java.util.Objects;
 
 public class DbSingleton {
 
-    private static DbSingleton instance;
+    private static volatile DbSingleton instance;
 
-    private DbSingleton() {}
+    private DbSingleton() {
+        if (!Objects.isNull(instance)) {
+            throw new RuntimeException("Use getInstance() method to create!");
+        }
+    }
 
     public static DbSingleton getInstance() {
         if (Objects.isNull(instance)) {
-            instance = new DbSingleton();
+            synchronized (DbSingleton.class) {
+                if (Objects.isNull(instance)) {
+                    instance = new DbSingleton();
+                }
+            }
         }
         return instance;
     }
