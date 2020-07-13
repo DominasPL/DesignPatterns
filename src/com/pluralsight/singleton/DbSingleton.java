8b8115869f1bff14baf6941b9b2ec72e07199df8
DominasPL <dominik-stepuch@wp.pl>
2020-07-13 11:58:34 +0200

    Added lazy loading to singleton class

--- src/com/pluralsight/singleton/DbSingleton.java
+++ src/com/pluralsight/singleton/DbSingleton.java
@@ -1,12 +1,17 @@
 package com.pluralsight.singleton;
 
+import java.util.Objects;
+
 public class DbSingleton {
 
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance;
 
     private DbSingleton() {}
 
     public static DbSingleton getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new DbSingleton();
+        }
         return instance;
     }
 }
